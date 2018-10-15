8785f7ca9a39e1fee448391b672e2410bbfb4fa1
samytichadou <samytichadou@gmail.com>
2018-10-15 10:51:17 +0200

    enhance shot render settings

--- set_shot_render_settings.py
+++ set_shot_render_settings.py
@@ -17,10 +17,11 @@ class ND_render_settings(bpy.types.Operator):
     state= bpy.props.EnumProperty(name="State", default="1", items=possible_states)
     proxy_percentage = bpy.props.IntProperty(name="Proxy Percentage", default=50, min=5, max=95)
     suppress_previous=bpy.props.BoolProperty(name="Suppress previous render", default=False)
+    relative=bpy.props.BoolProperty(name="Relative Path", default=False)
     
     @classmethod
     def poll(cls, context):
-        return True
+        return bpy.data.is_saved==True and context.scene.camera is not None
     
     def invoke(self, context, event):
         wm = context.window_manager
@@ -32,7 +33,10 @@ class ND_render_settings(bpy.types.Operator):
     def draw(self, context):
         layout = self.layout
         layout.prop(self, "state")
-        layout.prop(self, "suppress_previous")
+        row=layout.row()
+        row.prop(self, "suppress_previous")
+        if self.state!='2':
+            row.prop(self, "relative")
         if self.state!='0':
             layout.prop(self, 'proxy_percentage')
 
@@ -42,7 +46,8 @@ class ND_render_settings(bpy.types.Operator):
         pct=self.proxy_percentage
         coef=pct/100
         blend_path=bpy.data.filepath
-        name=scn.name+"_"
+        blend_name=os.path.splitext(blend_path)[0]
+        name=blend_name+"_"+scn.name+"_"+scn.camera.name
         
         #get shot number
         shot, cat, dir=return_shot_infos_from_path(blend_path)
@@ -55,21 +60,29 @@ class ND_render_settings(bpy.types.Operator):
         rd.use_freestyle = False
         rd.use_motion_blur = False
         
+        #relative path
+        if self.state!='2' and self.relative==True:
+            bpy.ops.file.make_paths_relative()
+        
         #playblast
         if self.state=='2':
-            rd.filepath=os.path.join(dir, "PLAYBLAST")
+            #create folder if needed
+            playblast_path=os.path.join(dir, os.path.join("004_PLAYBLAST", name))
+            rd.filepath=os.path.join(playblast_path, name+"_PLAYBLAST_####")
+            
             rd.image_settings.file_format = 'PNG'
             rd.image_settings.color_mode = 'RGBA'
             rd.image_settings.compression = 15
             rd.image_settings.color_depth = '8'
             rd.resolution_percentage = pct
-            rd.use_overwrite = True
+            #rd.use_overwrite = True
             activate_stamp_metadatas()
 
         #proxy
         elif self.state=='1':
-            proxy_path=os.path.join(dir, os.path.join(os.path.join("RENDER","PROXY"), scn.name))
-            rd.filepath=os.path.join(proxy_path, name+"RENDER_PROXY_####")
+            #create folder if needed
+            proxy_path=os.path.join(dir, os.path.join(os.path.join("005_RENDER","PROXY"), name))
+            rd.filepath=os.path.join(proxy_path, name+"_RENDER_PROXY_####")
             
             #suppress previous render if needed
             if self.suppress_previous==True:
@@ -85,8 +98,9 @@ class ND_render_settings(bpy.types.Operator):
 
         #render
         elif self.state=='0':
-            render_path=os.path.join(dir, os.path.join(os.path.join("RENDER","MASTERS"), scn.name))
-            rd.filepath=os.path.join(render_path, name+"RENDER_MASTER_####")
+            #create folder if needed
+            render_path=os.path.join(dir, os.path.join(os.path.join("005_RENDER","MASTERS"), name))
+            rd.filepath=os.path.join(render_path, name+"_RENDER_MASTER_####")
             
             #suppress previous render if needed
             if self.suppress_previous==True:
@@ -97,4 +111,3 @@ class ND_render_settings(bpy.types.Operator):
             rd.use_stamp=False
         
         return {"FINISHED"}
-        
