6c3f9c7cc9d5d64f2e24d6806afa4d94759e173b
samytichadou <samytichadou@gmail.com>
2018-11-30 18:00:05 +0100

    initialize editing folder

--- prefs.py
+++ prefs.py
@@ -15,11 +15,13 @@ class VCacheAddonPrefs(bpy.types.AddonPreferences):
             
     source=bpy.props.StringProperty(subtype="DIR_PATH", name='Source', default=r"C:\Users\GRAPHISTE 001\AppData\Roaming\Blender Foundation\Blender\2.79\scripts\addons\ND_addon_blender")
     destination=bpy.props.StringProperty(subtype="DIR_PATH", name='Destination', default=r"\\motionorama\MOTIONORAMA_DRIVE\----ELEMENTS\BLENDER\ne_pas_toucher_blender_common_scripts\scripts\addons\ND_addon_blender_share")
+    editing_folder=bpy.props.StringProperty(subtype="DIR_PATH", name='Editing Folder', default=r"M:\PROGRAM_33\NOTRE_DAME\002_PROD\001_PROJECTS\003_EDITING")
     spreadsheet_url=bpy.props.StringProperty(name='Spreadsheet URL', default=r"https://docs.google.com/spreadsheets/d/1-KIbJUdhPctKJBhjSaxth3IBxeJtPZMPDp7qmxA6S2E/")
             
     def draw(self, context):
         layout = self.layout
         layout.prop(self, 'prefs_folderpath')
+        layout.prop(self, 'editing_folder')
         layout.prop(self, 'spreadsheet_url')
         box=layout.box()
         box.label("Debug")
